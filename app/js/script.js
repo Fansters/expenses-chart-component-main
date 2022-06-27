109843a30609ad5825fef609023744d0ec0d86a5
fansters <edmundseizentals@gmail.com>
2022-06-27 14:36:57 +0300

    minor fixes, removed files, unnecessary code.

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,8 @@
 'use strict';
-
 async function fetchData() {
    const url = '../../data.json'
    const response = await fetch(url)
    const dataPoints = await response.json();
-   // console.log(dataPoints);
    return dataPoints;
 }
 
@@ -23,11 +21,11 @@ fetchData().then(dataP => {
 });
 
 const data = {
-   labels: ["mon", "tue", "wed", "thu", "fi", "sat", "sun"],
+   // labels: [],
    datasets: [
       {
-         label: "",
-         data: [17.45, 34.91, 52.36, 3.07, 23.39, 43.28, 25.48],
+         // label: " ",
+         // data: [],
          backgroundColor: [
             "rgb(236, 119, 95)",
             "rgb(236, 119, 95)",
@@ -40,11 +38,6 @@ const data = {
          borderWidth: 0,
          borderRadius: 3,
          borderSkipped: false,
-         // barPercentage: 0.5,
-         // barThickness: 6,
-         // maxBarThickness: 8,
-         // minBarLength: 2,
-         // data: [10, 20, 30, 40, 50, 60, 70],
          hoverBackgroundColor: "hsl(186, 34%, 60%)",
       },
    ],
@@ -56,10 +49,7 @@ const myChart = new Chart(ctx, {
    options: {
       maintainAspectRatio: false,
       responsive: true,
-      // interaction: {
-      // 	mode: "index",
-      // 	axis: "y",
-      // },
+     
       plugins: {
          tooltip: {
             // enabled:false,
